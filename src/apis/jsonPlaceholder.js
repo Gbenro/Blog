9cd3c9d5eb0459464771a4e04bf6ab82526759b6
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-15 20:28:22 -0600

    added actions

---
+++ src/apis/jsonPlaceholder.js
@@ -0,0 +1,6 @@
+import axois from 'axios'
+import Axios from 'axios'
+
+export default Axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com/posts'
+})
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import PostList from './PostList'
 
 export default class App extends Component {
   render () {
-    return <div className='ui container'>App</div>
+    return (
+      <div className='ui container'>
+        <PostList />
+      </div>
+    )
   }
 }
